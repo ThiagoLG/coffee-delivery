68cb97a6d54e512acf97e776d9f8dabe2c831b21
ThiagoLG <thiago.lg53@gmail.com>
2023-04-01 11:21:07 +0000

    configured theme colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,3 +1,23 @@
 export const defaultTheme = {
-    
+  colors: {
+    'yellow-dark': '#C47F17',
+    'yellow': '#DBAC2C',
+    'yellow-light': '#F1E9C9',
+    'purple-dark': '#4B2995',
+    'purple': '#8047F8',
+    'purple-light': '#EBE5F9',
+
+    'gray-100': '#F3F3F2',
+    'gray-200': '#EDEDED',
+    'gray-300': '#E6E5E5',
+    'gray-400': '#D7D5D5',
+    'gray-500': '#8D8686',
+    'gray-600': '#574F4D',
+    'gray-700': '#403937',
+    'gray-800': '#272221',
+
+    'white': '#FFFFFF',
+    'background': '#FAFAFA',
+  }
+
 }
