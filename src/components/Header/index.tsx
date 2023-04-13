0014a66504d2fc7fb746c6f770be72313c44d140
ThiagoLG <thiago.lg53@gmail.com>
2023-04-13 00:01:54 -0300

    Refactoring header + adding route links to cart and home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,25 +9,30 @@ import logo from '../../assets/logo.svg'
 import { defaultTheme } from '../../styles/themes/default'
 import { useContext } from 'react'
 import { CartContext } from '../../contexts/CartContext'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   const { cartItems } = useContext(CartContext)
 
   return (
     <HeaderContainer>
-      <img src={logo} alt="site logo" />
+      <NavLink to="/" title="home">
+        <img src={logo} alt="site logo" />
+      </NavLink>
 
-      <HeaderRegion>
-        <HeaderLocation>
+      <section className="headerActions">
+        <div className="baseButton location">
           <MapPin weight="fill" size={22} color={defaultTheme.purple} />
           <span>São Paulo, SP</span>
-        </HeaderLocation>
+        </div>
 
-        <HeaderCart>
-          <ShoppingCart weight="fill" size={22} color={defaultTheme.yellow} />
-          {cartItems.length ? <div>{cartItems.length}</div> : <></>}
-        </HeaderCart>
-      </HeaderRegion>
+        <NavLink to="/checkout" title="Checkout">
+          <div className="baseButton cart">
+            <ShoppingCart weight="fill" size={22} color={defaultTheme.yellow} />
+            {cartItems.length ? <div>{cartItems.length}</div> : <></>}
+          </div>
+        </NavLink>
+      </section>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,52 +9,52 @@ export const HeaderContainer = styled.nav`
   top: 0;
   background-color: ${(props) => props.theme.background};
   z-index: 10;
-`
 
-export const HeaderRegion = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 0.75rem;
-`
-
-const BaseHeaderButton = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 0.25rem;
-
-  height: 2.375rem;
-  padding: 0.5rem;
-  border-radius: 6px;
-`
-
-export const HeaderLocation = styled(BaseHeaderButton)`
-  font-size: 0.875rem;
-  color: ${(props) => props.theme['purple-dark']};
-  background-color: ${(props) => props.theme['purple-light']};
-`
-
-export const HeaderCart = styled(BaseHeaderButton)`
-  background-color: ${(props) => props.theme['yellow-light']};
-  position: relative;
-
-  div {
-    position: absolute;
-    border: 0;
-    background: ${(props) => props.theme['gray-800']};
-    border-radius: 50%;
-    width: 20px;
-    height: 20px;
-    color: white;
+  .headerActions {
     display: flex;
+    flex-direction: row;
     align-items: center;
-    justify-content: center;
-    font-weight: bold;
-    border: 1px solid black;
-    right: -25%;
-    bottom: -10%;
-    font-size: 12px;
+    gap: 0.75rem;
+
+    .baseButton {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      gap: 0.25rem;
+
+      height: 2.375rem;
+      padding: 0.5rem;
+      border-radius: 6px;
+
+      &.location {
+        font-size: 0.875rem;
+        color: ${(props) => props.theme['purple-dark']};
+        background-color: ${(props) => props.theme['purple-light']};
+      }
+
+      &.cart {
+        background-color: ${(props) => props.theme['yellow-light']};
+        position: relative;
+
+        div {
+          position: absolute;
+          border: 0;
+          background: ${(props) => props.theme['gray-800']};
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          color: white;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-weight: bold;
+          border: 1px solid black;
+          right: -25%;
+          bottom: -10%;
+          font-size: 12px;
+        }
+      }
+    }
   }
 `
