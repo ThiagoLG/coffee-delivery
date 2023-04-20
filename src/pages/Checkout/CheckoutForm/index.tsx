4094b6f2165de1a14ab9a5fc5ab91f589813fbdc
ThiagoLG <thiago.lg53@gmail.com>
2023-04-19 23:38:18 -0300

    Configured mast to zip code field + implemented validations to form

--- src/pages/Checkout/CheckoutForm/index.tsx
+++ src/pages/Checkout/CheckoutForm/index.tsx
@@ -9,6 +9,7 @@ import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect, useState } from 'react'
 
 interface AddressItem {
   cep: string
@@ -23,22 +24,29 @@ interface AddressItem {
   siafi: string
 }
 
-const deliveryValidationSchema = zod.object({
-  zip: zod.string().regex(/^\d{5}-\d{3}$/, 'Por favor, forneça um CEP válido'),
-  address: zod.string().nonempty('Por favor, informe o endereço'),
-  number: zod.number().min(1, 'Por favor, informe o número da residência.'),
-  complement: zod.string(),
-  district: zod.string().nonempty('Por favor, informe o bairro'),
-  city: zod.string().nonempty('Por favor, informe a cidade'),
-  UF: zod
-    .string()
-    .nonempty('Por favor, informe a UF')
-    .length(2, 'A UF deve ter 2 caracteres'),
-})
-
-type DeliveryInfosFormData = z.infer<typeof deliveryValidationSchema>
-
 export function CheckoutForm() {
+  const zipCodeRegexValidation = /^\d{5}-\d{3}$/
+
+  // #region form controllers
+  const deliveryValidationSchema = zod.object({
+    zip: zod
+      .string()
+      .regex(zipCodeRegexValidation, 'Por favor, forneça um CEP válido'),
+    address: zod.string().nonempty('Por favor, informe o endereço'),
+    number: zod.coerce
+      .number()
+      .min(1, 'Por favor, informe o número da residência.'),
+    complement: zod.string(),
+    district: zod.string().nonempty('Por favor, informe o bairro'),
+    city: zod.string().nonempty('Por favor, informe a cidade'),
+    UF: zod
+      .string()
+      .nonempty('Por favor, informe a UF')
+      .length(2, 'A UF deve ter 2 caracteres'),
+  })
+
+  type DeliveryInfosFormData = z.infer<typeof deliveryValidationSchema>
+
   const {
     register,
     handleSubmit,
@@ -47,29 +55,54 @@ export function CheckoutForm() {
   } = useForm<DeliveryInfosFormData>({
     resolver: zodResolver(deliveryValidationSchema),
   })
+  // #endregion
 
+  // #region states and hooks
+  const [addressInfosResult, setAddressInfosResult] = useState<AddressItem>()
+  // #endregion
+
+  // #region form handlers
   function handleSubmitForm(data: DeliveryInfosFormData) {
     console.log(data)
   }
 
-  async function getCEPInfos(event: React.ChangeEvent<HTMLInputElement>) {
-    const inputtedCEP = event.target.value
+  async function handleZipCodeChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) {
+    const zipCodeProvided = event.target.value
+    const zipCodeNumbersOnly = zipCodeProvided.replace(/\D/gi, '')
 
-    if (!errors.zip && inputtedCEP.length === 9) {
-      const cepWithoutMask = inputtedCEP.replace('-', '')
-      const url = `https://viacep.com.br/ws/${cepWithoutMask}/json/`
+    setValue('zip', zipCodeNumbersOnly.replace(/^(\d{5})(\d{1,3})+?$/, '$1-$2'))
 
-      const response = await fetch(url)
-      const data: AddressItem = await response.json()
-
-      setValue('address', data.logradouro)
-      setValue('district', data.bairro)
-      setValue('city', data.localidade)
-      setValue('UF', data.uf)
-
-      console.log(data)
-    }
+    if (zipCodeProvided.match(zipCodeRegexValidation))
+      getAddressFromZipCode(zipCodeNumbersOnly)
+    else clearAddressInfosToFields()
   }
+  // #endregion
+
+  // #region general functions
+  async function getAddressFromZipCode(zip: string) {
+    const url = `https://viacep.com.br/ws/${zip}/json/`
+
+    const response = await fetch(url)
+    const addressInfos: AddressItem = await response.json()
+
+    setAddressInfosResult(addressInfos)
+
+    setValue('address', addressInfos?.logradouro || '')
+    setValue('district', addressInfos?.bairro || '')
+    setValue('city', addressInfos?.localidade || '')
+    setValue('UF', addressInfos?.uf || '')
+  }
+
+  function clearAddressInfosToFields() {
+    if (!addressInfosResult) return
+    setValue('address', '')
+    setValue('district', '')
+    setValue('city', '')
+    setValue('UF', '')
+  }
+  // #endregion
 
   return (
     <CheckoutFormContainer onSubmit={handleSubmit(handleSubmitForm)}>
@@ -80,7 +113,7 @@ export function CheckoutForm() {
             placeholder="CEP"
             {...register('zip')}
             hasError={!!errors.zip}
-            onChange={getCEPInfos}
+            onChange={handleZipCodeChange}
             maxLength={9}
           />
           {errors.zip && <FormFieldError>{errors.zip.message}</FormFieldError>}
@@ -93,6 +126,7 @@ export function CheckoutForm() {
             type="text"
             placeholder="Rua"
             {...register('address')}
+            readOnly={!!addressInfosResult?.logradouro?.length}
             hasError={!!errors.address}
           />
           {errors.address && (
@@ -133,6 +167,7 @@ export function CheckoutForm() {
             type="text"
             placeholder="Bairro"
             {...register('district')}
+            readOnly={!!addressInfosResult?.bairro?.length}
             hasError={!!errors.district}
           />
           {errors.district && (
@@ -145,6 +180,7 @@ export function CheckoutForm() {
             type="text"
             placeholder="Cidade"
             {...register('city')}
+            readOnly={!!addressInfosResult?.localidade?.length}
             hasError={!!errors.city}
           />
           {errors.city && (
@@ -157,6 +193,7 @@ export function CheckoutForm() {
             type="text"
             placeholder="UF"
             {...register('UF')}
+            readOnly={!!addressInfosResult?.uf?.length}
             hasError={!!errors.UF}
           />
           {errors.UF && <FormFieldError>{errors.UF.message}</FormFieldError>}
